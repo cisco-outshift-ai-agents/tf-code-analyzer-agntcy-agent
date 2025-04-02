ddc85d0cd95e97dec1b23dd59cd895cceff2792b
Aditi Gupta <54046563+therealaditigupta@users.noreply.github.com>
2025-04-02 13:46:31 -0400

    feat: implement working docker-compose for gateway and code-analyzer
    Signed-off-by: Aditi Gupta <54046563+therealaditigupta@users.noreply.github.com>

--- app/agp/agp.py
+++ app/agp/agp.py
@@ -20,4 +20,4 @@ from agp_api.agent.agent_container import AgentContainer
 class AGPConfig:
     gateway_container = GatewayContainer()
     agent_container = AgentContainer()
-    remote_agent = "tf-code-analyzer"
+    remote_agent = "server"
