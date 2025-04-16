aca8aa68fbaa4eeb93d1ad2c7b673c08169b9fb5
brisacoder <dev@brisa.ai>
2025-04-16 11:39:37 -0700

    github_request is a dict
    - Cannot dereference with dot operator, needs to use get()
    
    Signed-off-by: brisacoder <dev@brisa.ai>

--- app/api/routes/stateless_runs.py
+++ app/api/routes/stateless_runs.py
@@ -20,20 +20,23 @@ import logging
 from datetime import datetime, timezone
 from http import HTTPStatus
 
-from agntcy_acp.acp_v0.models.run_stateless import \
-    RunStateless as ACPRunStateless
+from agntcy_acp.acp_v0.models.run_stateless import RunStateless as ACPRunStateless
 from agntcy_acp.acp_v0.models.run_status import RunStatus as ACPRunStatus
 from agntcy_acp.models import RunCreateStateless as ACPRunCreateStateless
-from agntcy_acp.models import \
-    RunWaitResponseStateless as ACPRunWaitResponseStateless
+from agntcy_acp.models import RunWaitResponseStateless as ACPRunWaitResponseStateless
 from fastapi import APIRouter, HTTPException, Request, status
 from fastapi.responses import JSONResponse
 
 from app.core.config import INTERNAL_ERROR_MESSAGE, get_llm_chain
 from app.core.github import GithubClient
 from app.graph.graph import StaticAnalyzerWorkflow
-from app.models.models import (Any, ErrorResponse, RunCreateStateless,
-                               RunCreateStatelessOutput, Union)
+from app.models.models import (
+    Any,
+    ErrorResponse,
+    RunCreateStateless,
+    RunCreateStatelessOutput,
+    Union,
+)
 
 router = APIRouter(tags=["Stateless Runs"])
 logger = logging.getLogger(__name__)
@@ -193,11 +196,12 @@ def wait_run_stateless_runs_wait_post(
             )
 
         # Initialize the Github client and download the repository.
-        github_client = GithubClient(github_request.github_token)
+        github_token = github_request.get("github_token")
+        github_client = GithubClient(github_token)
         try:
             file_path = github_client.download_repo_zip(
-                repo_url=github_request.repo_url,
-                branch=github_request.branch,
+                repo_url=github_request.get("repo_url"),
+                branch=github_request.get("branch"),
                 destination_folder=settings.DESTINATION_FOLDER,
             )
         except Exception as e:
